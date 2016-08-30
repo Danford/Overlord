c247c0f8834d80f03d1808b4f1946a568a812256
Cather Steincamp <catalystpoint@gmail.com>
2016-08-30 01:42:03 -0400

    Security Fix and functionality update
    - raw user input was being used in queries. Fixed that.
    - for json, reports fail for things that don't work.

--- modules/profile/post/friendmanagement.php
+++ modules/profile/post/friendmanagement.php
@@ -8,8 +8,8 @@
             $user->load_friends_list() ;
             
             if( ! $user->is_blocked( $uri[$pos] ) ) {
-                $db->insert( "INSERT INTO `profile_friendship_rq` SET `requestor`='".$user->id."', `requestee`='".$_POST['user']."'" ) ;
-                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$_POST['user']."', `type`='1', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
+                $db->insert( "INSERT INTO `profile_friendship_rq` SET `requestor`='".$user->id."', `requestee`='".$db->sanitize( $_POST['user'] )."'" ) ;
+                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$db->sanitize( $_POST['user'] )."', `type`='1', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
                 
                 $post->json_reply( 'SUCCESS' ) ;
 
@@ -17,25 +17,29 @@
             
             $post->json_reply( 'FAIL' ) ;
               
-            header( 'Location: /profile/'.$_POST['user'] );
+            header( 'Location: /profile/'.$db->sanitize( $_POST['user'] ) );
             die() ;
             
             
         case 'removefriend' :
             
-            $db->update( "DELETE FROM `profile_friendship` WHERE  
-                ( `friend1`='".$user->id."' AND `friend2`='".$_POST['user']."' )
+            $result = $db->update( "DELETE FROM `profile_friendship` WHERE  
+                ( `friend1`='".$user->id."' AND `friend2`='".$db->sanitize( $_POST['user'] )."' )
                     OR
-                ( `friend2`='".$user->id."' AND `friend1`='".$_POST['user']."' )" ) ;
+                ( `friend2`='".$user->id."' AND `friend1`='".$db->sanitize( $_POST['user'] )."' )" ) ;
             
-            $user->load_friends_list() ;
             
-
-            $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$_POST['user']."', `type`='3', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
+            if( $result == false or $result == 0 ){
+                $post->json_reply( 'FAIL' ) ;
+            } else {
             
-            $post->json_reply( 'SUCCESS' ) ;
+                $user->load_friends_list() ;
+                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$db->sanitize( $_POST['user'] )."', `type`='3', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
+                
+                $post->json_reply( 'SUCCESS' ) ;
+            }
             
-            header( 'Location: /profile/'.$_POST['user'] );
+            header( 'Location: /profile/'.$db->sanitize( $_POST['user'] ) );
             die() ;
             
         case 'confirmfriend' :
@@ -43,34 +47,34 @@
             // verify that there is a request
             
             if( $db->get_field( "SELECT count(*) FROM `profile_friendship_rq` WHERE 
-                `requestee`='".$user->id."' AND `requestor`='".$_POST['user']."'") == 1 ){
+                `requestee`='".$user->id."' AND `requestor`='".$db->sanitize( $_POST['user'] )."'") == 1 ){
                 
                 // remove friend request
                 
                 $db->update( "DELETE FROM `profile_friendship_rq` WHERE
-                    `requestee`='".$user->id."' AND `requestor`='".$_POST['user']."'" ) ;
+                    `requestee`='".$user->id."' AND `requestor`='".$db->sanitize( $_POST['user'] )."'" ) ;
             
                 
                 // add friendship
      
                 $db->insert( "INSERT INTO `profile_friendship` SET 
-                    `friend1`='".$user->id."', `friend2`='".$_POST['user']."', `timestamp`='".oe_time()."'" ) ;
+                    `friend1`='".$user->id."', `friend2`='".$db->sanitize( $_POST['user'] )."', `timestamp`='".oe_time()."'" ) ;
      
                 // add activity
                 
                 $st['user_id'] = $user->id ;
-                $st['ref'] = $_POST['user'] ;
+                $st['ref'] = $db->sanitize( $_POST['user'] ) ;
                 $st['type'] = 1 ;
                 $st['timestamp'] = oe_time() ;
                 
                 $db->insert( "INSERT INTO `user_activity` SET ".$db->build_set_string_from_array( $st ) ) ;
                 
-                $st['user_id'] = $_POST['user'] ;
+                $st['user_id'] = $db->sanitize( $_POST['user'] ) ;
                 $st['ref'] = $user->id ;
                 
                 $db->insert( "INSERT INTO `user_activity` SET ".$db->build_set_string_from_array( $st ) ) ;
 
-                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$_POST['user']."', `type`='2', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
+                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$db->sanitize( $_POST['user'] )."', `type`='2', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
                 
                 $user->load_friends_list() ;
                 
@@ -80,28 +84,28 @@
             
             $post->json_reply( 'FAIL' ) ;
             
-            header( 'Location: /profile/'.$_POST['user'] );
+            header( 'Location: /profile/'.$db->sanitize( $_POST['user'] ) );
             die() ;
             
         case 'denyfriend' :
             
             $result = $db->update( "DELETE FROM `profile_friendship_rq` WHERE
-                `requestor`='".$user->id."' AND `requestee`='".$_POST['user']."'" ) ;
+                `requestor`='".$user->id."' AND `requestee`='".$db->sanitize( $_POST['user'] )."'" ) ;
             
-            if( $result == false ){
+            if( $result == false or $result == 0 ){
                 $post->json_reply( 'FAIL' ) ;
             }
             
             $post->json_reply( 'SUCCESS' ) ;
             
             
-            header( 'Location: /profile/'.$_POST['user'] );
+            header( 'Location: /profile/'.$db->sanitize( $_POST['user'] ) );
             die() ;
             
         case 'cancelfriendrq' :
             
             $result = $db->update( "DELETE FROM `profile_friendship_rq` WHERE
-                `requestee`='".$user->id."' AND `requestor`='".$_POST['user']."'" ) ;
+                `requestee`='".$user->id."' AND `requestor`='".$db->sanitize( $_POST['user'] )."'" ) ;
             
             if( $result == false ){
                 $post->json_reply( 'FAIL' ) ;
@@ -109,28 +113,28 @@
         
             $post->json_reply( 'SUCCESS' ) ;
             
-            header( 'Location: /profile/'.$_POST['user'] );
+            header( 'Location: /profile/'.$db->sanitize( $_POST['user'] ) );
             die() ;
             
         case 'blockuser' :
             
             // add block entry 
             
-            $db->insert( "INSERT INTO `profile_block` SET `blocker`='".$user->id."' AND `blockee`='".$_POST['user']."'" ) ;
+            $db->insert( "INSERT INTO `profile_block` SET `blocker`='".$user->id."' AND `blockee`='".$db->sanitize( $_POST['user'] )."'" ) ;
             
             // remove any existing friendship
             
             $result = $db->update( "DELETE FROM `profile_friendship` WHERE  
-                ( `friend1`='".$user->id."' AND `friend2`='".$_POST['user']."' )
+                ( `friend1`='".$user->id."' AND `friend2`='".$db->sanitize( $_POST['user'] )."' )
                     OR
-                ( `friend2`='".$user->id."' AND `friend1`='".$_POST['user']."' )" ) ;
+                ( `friend2`='".$user->id."' AND `friend1`='".$db->sanitize( $_POST['user'] )."' )" ) ;
  
             
             if( $result != false ){
                 
                 // add activity for BLOCKED user so that they will have their friend/block list updated
                 
-                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$_POST['user']."', `type`='0', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
+                $db->insert( "INSERT INTO `user_notification` SET `user_id`='".$db->sanitize( $_POST['user'] )."', `type`='0', ref='".$user->id."', `timestamp`='".oe_time()."'") ;
                 $user->load_friends_list() ;
             
                 $post->json_reply( 'SUCCESS' ) ;
