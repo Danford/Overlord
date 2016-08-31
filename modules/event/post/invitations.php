3c524b8caafe2392e96bccfb39c189dfdff7ce94
Cather Steincamp <catalystpoint@gmail.com>
2016-08-31 04:36:32 -0400

    complete overhaul of event invitations
    This one was a mess.

--- modules/event/post/invitations.php
+++ modules/event/post/invitations.php
@@ -7,46 +7,91 @@ if( preg_match( '/^[0-9]*$/', $_POST['event_id'] ) != 0 ){
 switch( $_POST['oe_formid'] ){
 
     case "invite":
-
-        verify_invitations( $_POST["invitees"] ) ;
-
-        if( $event->is_organiser() or $event->type == 1 ){
-
-            // create approved invitation
-
-            foreach( $_POST["invitees"] as $invited ){
-
-                $db->insert( "INSERT INTO `event_invite` SET `event_id`='".$event->id."', `user_id`='".$invited."', rsvp='0', 
-                    type='0', `invited_by`='".$user->id."'" ) ;
+        
+        if( $event->group == 0 or $event->group == null ){
+        
+            // not a group event
+        
+        
+            if( $event->is_organiser() or $event->type == 1 ){
+        
+                // create approved invitation
+        
+                $type = 0 ;
+        
+            } else {
+        
+                // create an invitation for a moderator to approve
+        
+                $type = 1 ;
             }
-
+        
         } else {
-
-            // create an invitation for a moderator to approve
-
-            foreach( $_POST["invitees"] as $invited ){
-
-                $db->insert( "INSERT INTO `group_invite` SET `group_id`='".$group->id."', `user_id`='".$invited."', rsvp='0',
-                    type='1', `invited_by`='".$user->id."'" ) ;
+        
+            // group event
+        
+            include( $oe_modules['group']."lib/group_minion.php" );
+        
+            $group = new group_minion( $event->group ) ;
+        
+            if( ! $group->is_member( $user->id() ) ){ $post->json_reply('FAIL') ; }
+        
+            if( $group->is_organiser() or $group->type == 1 ){
+        
+                // create approved invitation
+        
+                $type = 0 ;
+        
+            } else {
+        
+                // create an invitation for a moderator to approve
+        
+                $type = 1 ;
             }
+        
+        
+        }
+        
+        foreach( $_POST["invitees"] as $invited ){
+        
+            $db->insert( "INSERT INTO `event_invite` SET `event_id`='".$event->id."', `user_id`='".$invited."', rsvp='0',
+                    type='".$type."', `invited_by`='".$user->id."'" ) ;
         }
         
         $post->json_reply('SUCCESS') ;
         header( "Location: ".$_POST['oe_return'] ) ;
         die();
+        
 
     case 'approve_invitation':
 
-        if( $group->is_moderator() ){
-            verify_invitations( $_POST["invitees"] ) ;
-            $db->update( "UPDATE `event_invite` SET `type`='0' WHERE `group_id`='".$group->id."'
-                AND `user_id` IN (".render_array_as_string($_POST["invitees"]).")" ) ;
-            $post->json_reply('SUCCESS') ;
+        if( $event->group == null or $event->group == 0 ){
+        
+        	if( ! $event->is_organiser() ){
+        		$post->json_reply( 'FAIL' ) ;
+        		die() ;
+        	}
+        
+        } else {
+        
+        	include( $oe_modules['group']."lib/group_minion.php" ) ;
+       
+       		$group = new group_minion( $event->group ) ;
+        
+        	if( ! $group->is_moderator() ){
+        		$post->json_reply( 'FAIL' ) ;
+        		die() ;
+        	}
         }
         
-        $post->json_reply('ERROR', 'unauthorised') ;
-        header( "Location: ".$_POST['oe_return'] ) ;
-        die();
+        
+        verify_invitations( $_POST["invitees"] ) ;
+        $db->update( "UPDATE `event_invite` SET `type`='0' WHERE `event_id`='".$event->id."'
+            AND `user_id` IN (".render_array_as_string($_POST["invitees"]).")" ) ;
+        $post->json_reply('SUCCESS') ;
+    
+    	header( "Location: ".$_POST['oe_return'] ) ;
+    	die();
 
     case 'request':
 
@@ -72,9 +117,11 @@ die( 'not routed by module' ) ;
 
 
 function verify_invitations( $list ){
-
+    
+    global $post ;
+    
     foreach( $list as $item ){
-        if( preg_match( '/^[0-9]*$/', $item ) == 0 ){ die() ; }
+        if( preg_match( '/^[0-9]*$/', $item ) == 0 ){ $post->json_reply( 'FAIL') ; die() ; }
     }
 
 }
