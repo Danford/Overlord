94c16fa4fcac6365cd62639107f85c550d48ef4f
Cather Steincamp <catalystpoint@gmail.com>
2016-09-11 04:24:26 -0400

    modified plugin.conf.php to support API calls
    commented out unimplemented conf for now.

--- mods/profile/conf/plugin.conf.php
+++ mods/profile/conf/plugin.conf.php
@@ -1,5 +1,20 @@
 <?php
 
+if( ! isset( $profile ) ){
+    
+    // this is being accessed via a plugin API
+    
+    $profile = new_profile( $basemoduleID, min ) ;
+    
+    if( $profile->name == false ){
+        
+        $post->json_reply("FAIL") ;
+        die() ;
+    }
+    
+}
+
+
 $parent[0] = [ 'type' => 'profile', 'id' => $profile->id ] ;
 $plugins[0] = [ 'photo', 'writing', `albums`] ;
 
@@ -9,10 +24,11 @@ $pluginconf[0]['admin'] = ( $profile->id == $user->id ) ;
         // only the user can upload/edit content via plugins on the profile
 
 $pluginconf[0]['photo']['useAlbums'] = false ;
+//$pluginconf[0]['photo']['table'] = 'photo' ;
 //$pluginconf[0]['photo']['view'] = 'photo' ;
 //$pluginconf[0]['photo']['folder'] = ul_img_dir ;
 
-$pluginconf[0]['writing']['use_albums'] = true ;
-$pluginconf[0]['writing']['view'] = 'writing_profile' ;
+//$pluginconf[0]['writing']['use_albums'] = true ;
+//$pluginconf[0]['writing']['view'] = 'writing_profile' ;
 
-$pluginconf[0]['album']['view'] = 'album_profile' ;
+//$pluginconf[0]['album']['view'] = 'album_profile' ;
