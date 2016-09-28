97738254afd7dfdbf12b07d4f13c89e7e1b339b7
Danford <Joshua.Danford@gmail.com>
2016-09-28 01:34:24 -0400

    Cleanup of upload.php Testing writing view_all.php Sample write.php Fixed bug in writing plugin.php which was preventing /writing/write/ from loading the proper page but there's still an issue as /profile/[id]/writing works and /profile/[id]/writing/ doesn't

--- plugins/writing/pages/write.php
+++ plugins/writing/pages/write.php
@@ -9,13 +9,31 @@
  *      copy - text/html
  *      privacy - int
  * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
  */
 
-echo "this is write.php";
+include(oe_frontend."page_minion.php");
+include(oe_lib."form_minion.php");
+
+$page = new page_minion("New Writing");
+
+$page->header();
+$form = new form_minion("writing", "write");
+
+global $privacyoptions;
+?>
+
+<div id="upload-photo-form">
+	<?php $form->header(); ?>
+	<p>Privacy:	<?php $form->select("privacy", $privacyoptions); ?></p>
+	<p>Title: <?php $form->text_field("title"); ?></p>
+	<p>Subtitle: <?php $form->text_field("subtitle"); ?></p>
+	<p>Copy:</p>
+	<p><?php $form->textarea_field("copy"); ?></p>
+	
+	<?php $form->submit_button("Submit Writing"); ?>
+</div>
+
+<?php 
+	$form->footer(); // it's not just cosmetic, it does session cleanup.
+    $page->footer();
+?>
