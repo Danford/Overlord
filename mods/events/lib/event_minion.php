d4bfab93e794c5bb6da82e031a690e1e69178f67
Cather Steincamp <catalystpoint@gmail.com>
2016-10-07 15:11:55 -0400

    beginning of event module/plugin.

---
+++ mods/events/lib/event_minion.php
@@ -0,0 +1,25 @@
+<?php
+
+class event_minion {
+        
+        var $id ;
+        var $title ;
+        var $privacy ;
+        
+    function __construct( $id ){
+        
+        global $user ;
+        global $db ;
+        
+        
+        
+    }
+    
+    function get_uninvitable(){
+        
+    }
+    
+    function get_blocked(){
+        
+    }
+}
