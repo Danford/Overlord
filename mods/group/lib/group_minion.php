e9eae49e1abb81507aeda4aac5fcc5473fa985b3
Cather Steincamp <catalystpoint@gmail.com>
2016-10-11 23:54:35 -0400

    updates to group minion and work on event module/plugin.

--- mods/group/lib/group_minion.php
+++ mods/group/lib/group_minion.php
@@ -257,7 +257,8 @@ class group_minion {
         $db->query( "SELECT `invitee` FROM `invitations`
                     WHERE `module`='group'
                         AND `module_item_id`='".$this->id."'
-                            AND `user`='".$user->id."'" ) ;
+                        AND `user`='".$user->id."'
+                        AND `expired` = '0'" ) ;
 
         
         while( ( $i = $db->field() ) != false ){
@@ -268,7 +269,6 @@ class group_minion {
         }
         
         return $response ;
-        
     }
     
     function get_blocked(){
