9ad3f782645972f1f169a3f842c9662776c2ff93
Danford <Joshua.Danford@gmail.com>
2016-10-21 01:27:39 -0400

    Fixed bug with profile adding tiles works again.

--- js/isotope.js
+++ js/isotope.js
@@ -2,10 +2,86 @@
  * 
  */
 
-$(function() {
-	// add css loading spinner after all tile images with the loading class
-	$('.tile > img.loading').after("<div align='center' class='cssload-fond'><div class='cssload-container-general'><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_1'></div></div><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_2'></div></div><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_3'></div></div><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_4'></div></div></div></div>");
+var $selectedTile = null;
+
+var $filterButtons = $('.filters .button');
+
+
+function OnPhotoClick($photo) {
+	$photo.toggleClass('grid-item--large');
+}
+
+function OnWritingClick($writing) {
+	$writing.toggleClass('grid-item--large');
+	$writing.children('#excerpt').toggleClass('hidden');
+	$writing.children('#full').toggleClass('hidden');
+	$grid.isotope('layout');
+}
+
+function OnGroupClick($group) {
 	
+}
+
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
+
+function updateFilterCounts()  {
+  // get filtered item elements
+  var itemElems = $grid.isotope('getFilteredItemElements');
+  var $itemElems = $( itemElems );
+  $filterButtons.each( function( i, button ) {
+    var $button = $( button );
+    var filterValue = $button.attr('data-filter');
+    if ( !filterValue ) {
+      // do not update 'any' buttons
+      return;
+    }
+    var count = $itemElems.filter( filterValue ).length;
+    $button.find('.filter-count').text( '(' + count +')' );
+  });
+}
+
+var requestProcessing = false;
+function AddTile(name, url) {
+
+	// do not allow multiple request to process at the same time.
+	if (requestProcessing == true)
+		return;
+
+	requestProcessing = true;
+	$newTile = $('<div id="'+ name +'" class="grid-item grid-item--large tile destructable"><h2>'+ name +'</h2><div align="center" class="cssload-fond"><div class="cssload-container-general"><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_1"></div></div><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_2"></div></div><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_3"></div></div><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_4"></div></div></div></div></div>');
+	$('.grid').prepend($newTile).isotope('reloadItems').isotope({ sortBy: 'original-order' });
+
+	$newTile.trigger('click');
+	
+	var jqxhr = $.get(url + "?ajax", function() {
+		
+	})
+	.done(function(data) {
+		$newTile.append(data);
+	})
+	.fail(function() {
+		$newTile.append("Error ajax failed...")
+	})
+	.always(function() {
+		$newTile.find('.cssload-fond').toggleClass('hidden');
+		$grid.isotope('layout');
+		$selectedTile = $newTile;
+		requestProcessing = false;
+	});
+}
+
+function FillTile(response) {
+	$newTile.append(response);
+}
+
+$(function() {
 	$grid = $('.grid').isotope({
 		// options
 		itemSelector: '.grid-item',
@@ -23,10 +99,9 @@ $(function() {
 			columnWidth: '.grid-sizer'
 		},
 	});
-	
-	var $selectedTile = null;
-	
-	var $filterButtons = $('.filters .button');
+
+	// add css loading spinner after all tile images with the loading class
+	$('.tile > img.loading').after("<div align='center' class='cssload-fond'><div class='cssload-container-general'><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_1'></div></div><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_2'></div></div><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_3'></div></div><div class='cssload-internal'><div class='cssload-ballcolor cssload-ball_4'></div></div></div></div>");
 	
 	updateFilterCounts();
 	
@@ -98,78 +173,4 @@ $(function() {
 	
 		$selectedTile = $(this);
 	});
-	
-	function OnPhotoClick($photo) {
-		$photo.toggleClass('grid-item--large');
-	}
-	
-	function OnWritingClick($writing) {
-		$writing.toggleClass('grid-item--large');
-		$writing.children('#excerpt').toggleClass('hidden');
-		$writing.children('#full').toggleClass('hidden');
-		$grid.isotope('layout');
-	}
-	
-	function OnGroupClick($group) {
-		
-	}
-	
-	// flatten object by concatting values
-	function concatValues( obj ) {
-	  var value = '';
-	  for ( var prop in obj ) {
-	    value += obj[ prop ];
-	  }
-	  return value;
-	}
-	
-	function updateFilterCounts()  {
-	  // get filtered item elements
-	  var itemElems = $grid.isotope('getFilteredItemElements');
-	  var $itemElems = $( itemElems );
-	  $filterButtons.each( function( i, button ) {
-	    var $button = $( button );
-	    var filterValue = $button.attr('data-filter');
-	    if ( !filterValue ) {
-	      // do not update 'any' buttons
-	      return;
-	    }
-	    var count = $itemElems.filter( filterValue ).length;
-	    $button.find('.filter-count').text( '(' + count +')' );
-	  });
-	}
-	
-	var requestProcessing = false;
-	function AddTile(name, url) {
-	
-		// do not allow multiple request to process at the same time.
-		if (requestProcessing == true)
-			return;
-	
-		requestProcessing = true;
-		$newTile = $('<div id="'+ name +'" class="grid-item grid-item--large tile destructable"><h2>'+ name +'</h2><div align="center" class="cssload-fond"><div class="cssload-container-general"><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_1"></div></div><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_2"></div></div><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_3"></div></div><div class="cssload-internal"><div class="cssload-ballcolor cssload-ball_4"></div></div></div></div></div>');
-		$('.grid').prepend($newTile).isotope('reloadItems').isotope({ sortBy: 'original-order' });
-	
-		$newTile.trigger('click');
-		
-		var jqxhr = $.get(url + "?ajax", function() {
-			
-		})
-		.done(function(data) {
-			$newTile.append(data);
-		})
-		.fail(function() {
-			$newTile.append("Error ajax failed...")
-		})
-		.always(function() {
-			$newTile.find('.cssload-fond').toggleClass('hidden');
-			$grid.isotope('layout');
-			$selectedTile = $newTile;
-			requestProcessing = false;
-		});
-	}
-	
-	function FillTile(response) {
-		$newTile.append(response);
-	}
 });
