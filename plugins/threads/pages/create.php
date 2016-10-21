e27be49893a074cb70749ac51fd90d3417a26e80
Danford <Joshua.Danford@gmail.com>
2016-10-20 21:56:44 -0400

    Added create.php for threads.

--- plugins/threads/pages/create.php
+++ plugins/threads/pages/create.php
@@ -15,4 +15,53 @@
  * 
  */
 
-echo 'create.php';
+include(oe_frontend."page_minion.php");
+include(oe_lib."form_minion.php");
+
+if (!isset($_GET['ajax']))
+{
+	$page = new page_minion("Create Thread");
+	
+	$page->addjs('/js/tinymce/tinymce.min.js');
+	$page->addjs('/js/invoketinymce.js');
+	
+	$page->header();
+}
+
+$form = new form_minion("create", "thread");
+
+global $privacyoptions;
+?>
+
+<div id="create-group-form">
+	<?php $form->header(); ?>
+	<p>Name: <?php $form->text_field("title"); ?></p>
+	<p>Description: <?php $form->text_field("detail"); ?></p>
+	<?php if ($group->membership == 2) : ?>
+	<p>Sticky: <?php $form->checkbox("sticky"); ?></p>
+	<p>Locked: <?php $form->checkbox("locked"); ?></p>
+	<?php endif; ?>
+	
+	<?php $form->submit_button("Create Thread"); ?>
+</div>
+
+<?php 
+	$form->footer(); // it's not just cosmetic, it does session cleanup.
+if (!isset($_GET['ajax']))
+{
+    $page->footer();
+}
+else
+{
+?>
+<script>
+$(function() {
+	tinymce.remove();
+	tinymce.init({selector:'textarea'});
+	
+	$('.grid').isotope('layout');
+});
+</script>
+<?php
+}
+?>
